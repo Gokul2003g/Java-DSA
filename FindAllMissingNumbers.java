0a5f3623166c59b72038953727b15d0daa63aebc
Gokul <gokul2003g@gmail.com>
2022-04-23 16:33:13 +0530

    Solution to 2 more questions added

---
+++ FindAllMissingNumbers.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllMissingNumbers {
+    public List<Integer> findMissingNums(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+
+        // find missing numbers
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}

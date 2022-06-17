8db2a90d3e146283b2f7d615bbf4c4c033d09349
Gokul <gokul2003g@gmail.com>
2022-06-18 00:01:38 +0530

    Used fibonacci recursion formula to find fibonacci numbers

--- recursion/fiboFormula.java
+++ recursion/fiboFormula.java
@@ -2,10 +2,12 @@ package com.company.recursion;
 
 public class fiboFormula {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 50; i++) {
+            System.out.println(fibo(i));
+        }
     }
 
-    static int fibo(int n) {
-      
+    static long fibo(int n) {
+      return (long)((Math.pow(((1 + Math.sqrt(5)) /2 ), n) - Math.pow(((1 - Math.sqrt(5)) / 2), n)) / Math.sqrt(5));
     }
 }

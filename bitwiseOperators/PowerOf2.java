ac6ee2c540b31672b6adff5eab89b815e6013a71
Gokul <gokul2003g@gmail.com>
2022-06-19 14:50:38 +0530

    Check if a number is power of 2 or not

--- bitwiseOperators/PowerOf2.java
+++ bitwiseOperators/PowerOf2.java
@@ -2,12 +2,15 @@ package com.company.bitwiseOperators;
 
 public class PowerOf2 {
     public static void main(String[] args) {
-        int n = 30;
+        int n = 2;
         System.out.println(isPowerOf2(n));
     }
 
     static boolean isPowerOf2(int a) {
-        if ((a & (a-1)) == 0) {
+        if ( a == 0) {
+            return false;
+        }
+         else if ((a & (a-1)) == 0) {
             return true;
         } else {
             return false;

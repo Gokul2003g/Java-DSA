554b9a0fa051b353433c583253a572271aff9202
Gokul <gokul2003g@gmail.com>
2022-06-19 15:01:15 +0530

    Calculate power using right shift

---
+++ bitwiseOperators/Power.java
@@ -0,0 +1,19 @@
+package com.company.bitwiseOperators;
+
+public class Power {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 4;
+        int ans = 1;
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                ans *= base;
+            }
+
+            base *= base;
+            power >>= 1;
+        }
+        System.out.println(ans);
+    }
+}

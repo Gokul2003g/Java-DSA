ecc7d9fbd15ab2cccef57a7695c5e3ff74ded4b3
Gokul2003g <gokul2003g@gmail.com>
2022-07-25 18:27:56 +0530

    Newton Rahpson Method to find square root

---
+++ Math/NewtonRaphsonMethod.java
@@ -0,0 +1,24 @@
+package com.company.Math;
+
+public class NewtonRaphsonMethod {
+    public static void main(String[] args) {
+        System.out.println(sqrt(10000));
+    }
+    static double sqrt (double n) {
+        double x = n;
+        double root;
+
+        while (true) {
+            root = 0.5 * (x + (n/x));
+
+            if(Math.abs(root - x) < 0.00001) {
+                break;
+            }
+
+            x = root;
+        }
+
+        return root;
+    }
+}
+

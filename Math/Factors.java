9dab184fe80cf5215ef9e7cf7e154c25e5e2194b
Gokul2003g <gokul2003g@gmail.com>
2022-07-25 19:23:55 +0530

    Finding factor using square root

---
+++ Math/Factors.java
@@ -0,0 +1,37 @@
+package com.company.Math;
+
+import java.util.ArrayList;
+
+public class Factors {
+    public static void main(String[] args) {
+        int n = 40;
+        factors2(n);
+    }
+
+    //O(n)
+    static void factors1(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    //O(sqrt(n))
+    static void factors2(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n/i == i) {
+                    System.out.print(i);
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n/i);
+                }
+            }
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}

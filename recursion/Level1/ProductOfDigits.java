dfd6ed09bf975fa72c00f0eac3dd04592b5ff803
Gokul2003g <gokul2003g@gmail.com>
2022-07-26 15:40:36 +0530

    Finding product of digits using recursion

---
+++ recursion/Level1/ProductOfDigits.java
@@ -0,0 +1,14 @@
+package com.company.recursion.Level1;
+
+public class ProductOfDigits {
+    public static void main(String[] args) {
+        System.out.println(prod(324));
+    }
+
+    static int prod(int n) {
+        if (n % 10 == n) {
+            return n;
+        }
+        return (n % 10) * prod(n / 10);
+    }
+}
